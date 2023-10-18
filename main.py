4e64c15f361853abbdde5de6173c5f9921cf3e28
rbeze <rbeze09@gmail.com>
2023-10-18 15:42:32 -0300

    Added vm in .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("Olá mundo!")
-print("Hello World!") # Printa hello world
-print("MMMMM")
+from flask import Flask
 
+# Crie uma instância do aplicativo Flask
+app = Flask(__name__)
+
+# Defina uma rota e uma função de visualização (view function)
+@app.route('/')
+def hello_world():
+    return 'Olá, mundo! Este é o meu servidor web em Flask.'
+
+if __name__ == '__main__':
+    # Inicie o servidor Flask na porta 5000 (por padrão)
+    app.run()
